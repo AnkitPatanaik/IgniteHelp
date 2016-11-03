ab067769938143a5d1d30d4098af500ce0caeb06
AnkitPatanaik <ankit@purdue.edu>
2016-11-03 11:14:19 -0400

    Clean

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 //my api key
-var API_KEY = '12286b90480b8599e5a08ffbf87d0caf' //to hit The Movie Database API
+const API_KEY = '12286b90480b8599e5a08ffbf87d0caf' //to hit The Movie Database API
 
 //import packages
 var http = require("https");
-var Promise = require("bluebird");
 var request =  require('request'); //Makes it easy for us to get or post to different urls
 var rp = require('request-promise');
 var express = require('express');
 app = express();
 
-
-//TODO -> first route
 app.get('/movies', function (req, res) {
 	var overview = getMovie("storks") 
-		//console.log(req);
-		console.log("The overview of the movie is: " + overview);
-		res.send(overview);
+	console.log("The overview of the movie is: " + overview);
+	res.send(overview);
 })
 
 //home page
@@ -41,9 +37,9 @@ function getMovie(movie) {
 	  		headers: { 'content-type': 'application/json', 'User-Agent': 'Request-Promise'
 	  		},
 	  	body: {},
-	  	json: true };
-
-		return rp(options) //return a promise
+	  	json: true 
+	};
+	return rp(options) //return a promise
 }
 
 var x = getMovie("Storks"); //this is a promise
@@ -53,5 +49,3 @@ x.then(function(value) {  //handle results of promise
   }, function(err) {
   	console.log(err);
 });
-
-//console.log("YO " + getMovie("Storks", test("ay it's me")));
